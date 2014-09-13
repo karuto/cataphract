1d235ba10414cd9a8cb5a46c401ca03ba45f9110
Karuto Z <iamkaruto@gmail.com>
2014-09-12 19:59:38 -0700

    Brought server to request handler to render diff content

--- router.js
+++ router.js
@@ -1,9 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   console.log("About to route a request for " + pathname);
-  if (typeof handle[pathname] == 'function') {
-    handle[pathname]();
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response);
   } else {
     console.log("No request handler function found");
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 NOT FOUND");
+    response.end();
   }
 }
 
